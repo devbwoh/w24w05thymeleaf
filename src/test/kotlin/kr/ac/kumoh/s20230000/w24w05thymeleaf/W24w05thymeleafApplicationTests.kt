7fe9798a8287701c0c74c6079b173af34fee2926
BWOH <bwoh@outlook.kr>
2024-10-01 22:52:57 +0900

    🎉 random template 시작

---
+++ src/test/kotlin/kr/ac/kumoh/s20230000/w24w05thymeleaf/W24w05thymeleafApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20230000.w24w05thymeleaf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w05thymeleafApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
